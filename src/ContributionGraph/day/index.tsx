91a572103adb7cb2ca49d00e0e20cdbddde56fc0
BilimAdisov <112559246+BilimAdisov@users.noreply.github.com>
2023-08-21 03:18:28 +0600

    sosa

---
+++ src/ContributionGraph/day/index.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import styles from "./style.module.css";
+
+const DayComponent = ({ date, value }: { date: string; value: any }) => {
+  const [color, setColor] = useState("rgba(237, 237, 237, 1)");
+  useEffect(() => {
+    if (value >= 1 && value <= 9) {
+      setColor("rgba(172, 213, 242, 1)");
+    } else if (value >= 10 && value <= 19) {
+      setColor("rgba(127, 168, 201, 1)");
+    } else if (value >= 20 && value <= 29) {
+      setColor("rgba(82, 123, 160, 1)");
+    } else if (value >= 30) {
+      setColor("rgba(37, 78, 119, 1)");
+    } else {
+      setColor("rgba(237, 237, 237, 1)");
+    }
+  }, [value]);
+  return (
+    <div
+      style={{
+        background: `${color}`,
+      }}
+      className={styles.day}
+    >
+      <div className={styles.popover}>
+        <div className={styles.content}>
+          <p className={styles.phara}>{value} contributions</p>
+          <p className={styles.phara}>{date}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { DayComponent };
--- src/ContributionGraph/index.tsx
+++ src/ContributionGraph/index.tsx
@@ -1,5 +1,15 @@
-const ContributionGraph = () => {
-  return <div>dfsd</div>;
+import { DayComponent } from "./day";
+import styles from "./style.module.css";
+
+const ContributionGraph = ({ data }: any) => {
+  return (
+    <div className={styles.container_data}>
+      {data &&
+        Object.entries(data).map(([key, value]: any) => (
+          <DayComponent key={key} date={key} value={value} />
+        ))}
+    </div>
+  );
 };
 
 export { ContributionGraph };
